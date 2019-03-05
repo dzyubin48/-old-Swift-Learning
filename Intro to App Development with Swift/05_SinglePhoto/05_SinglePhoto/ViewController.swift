45ee2bc6ccba6fe14fcf7e2d311034af68f04774
Dzyubin Danila <dzyubin@tutu.ru>
2019-03-05 13:45:16 +0300

    Intro to app development. 5. SinglePhoto

---
+++ Intro to App Development with Swift/05_SinglePhoto/05_SinglePhoto/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  05_SinglePhoto
+//
+//  Created by Dzyubin Danila on 05/03/2019.
+//  Copyright © 2019 Dzyubin Danila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+

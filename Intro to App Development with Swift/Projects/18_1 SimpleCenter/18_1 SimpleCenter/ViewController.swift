654e20871b2524e59765b06d619c5ad7bcf5135c
Dzyubin Danila <dzyubin@tutu.ru>
2019-03-10 19:00:13 +0300

    Intro to app development. 18.1. Adaptive User Interface. SimpleCenter app

---
+++ Intro to App Development with Swift/Projects/18_1 SimpleCenter/18_1 SimpleCenter/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  18_1 SimpleCenter
+//
+//  Created by Dzyubin Danila on 10/03/2019.
+//  Copyright © 2019 Dzyubin Danila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+

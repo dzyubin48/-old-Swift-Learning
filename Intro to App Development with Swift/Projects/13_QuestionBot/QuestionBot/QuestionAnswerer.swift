c171cefb41831c9b23183aecdcc1914ba979abdf
Dzyubin Danila <dzyubin@tutu.ru>
2019-03-10 14:26:24 +0300

    Intro to app development. 13. Question bot

---
+++ Intro to App Development with Swift/Projects/13_QuestionBot/QuestionBot/QuestionAnswerer.swift
@@ -0,0 +1,26 @@
+// My code
+
+struct MyQuestionAnswerer {
+    func responseTo(question: String) -> String {
+
+        let lowerQuestion = question.lowercased()
+
+        if lowerQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        } else if lowerQuestion.hasPrefix("where") {
+            return "To the North!"
+        } else if lowerQuestion.hasPrefix("hello") {
+            return "Why, hello there!"
+        } else {
+
+            let defaultNumber = question.count % 2
+
+            if defaultNumber == 0 {
+                return "That really depends"
+            } else {
+                return "Ask me again tomorrow"
+            }
+
+        }
+    }
+}

effab9f82ec08b7d2fa016eee503d6469c277f1c
Dzyubin Danila <dzyubin@tutu.ru>
2019-03-10 17:14:33 +0300

    Intro to app development. 16. Chat bot

---
+++ Intro to App Development with Swift/Projects/16_ChatBot/ChatBot/ConversationDataSource.swift
@@ -0,0 +1,34 @@
+// My code
+
+import Foundation
+
+class ConversationDataSource {
+
+    /// The number of Messages in the conversation
+    var messageCount: Int {
+        return messages.count
+    }
+
+    // Collection of Messages
+    var messages = [openingLine]
+    
+    /// Add a new question to the conversation
+    func add(question: String) {
+        let message = Message(date: Date(), text: question, type: .question)
+        messages.append(message)
+        print("Asked to add question: \(question)")
+    }
+    
+    /// Add a new answer to the conversation
+    func add(answer: String) {
+        let message = Message(date: Date(), text: answer, type: .answer)
+        messages.append(message)
+        print("Asked to add answer: \(answer)")
+    }
+    
+    /// The Message at a specific point in the conversation
+    func messageAt(index: Int) -> Message {
+        print("Asking for message at index \(index)")
+        return messages[index]
+    }
+}
